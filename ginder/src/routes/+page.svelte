75773a52ee352d8ffa57a8572652c6a7864e6131
Boushra Bettir <116927138+boushrabettir@users.noreply.github.com>
2023-07-28 21:13:52 -0700

    Removed client id. Migrated to .env

--- ginder/src/routes/+page.svelte
+++ ginder/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
    
-    const CLIENT_ID = "f4b411c4d7b6b50ef40e";
+    const CLIENT_ID = "";
     const login_with_github = () => {
         window.location.assign(`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`)
     }
