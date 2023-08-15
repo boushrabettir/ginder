3b090d3baf17e8231eaca6c98f38fd28110a7c84
Boushra Bettir <116927138+boushrabettir@users.noreply.github.com>
2023-08-14 21:32:02 -0700

    Update Loading.svelte

--- ginder/src/lib/components/Loading/Loading.svelte
+++ ginder/src/lib/components/Loading/Loading.svelte
@@ -7,7 +7,7 @@
     <link href="/dist/output.css" rel="stylesheet">
 </head>
 
-<div class="bg-zinc-200 rounded-md text-center p-10 absolute z-10 center">
+<div class="container bg-zinc-200 rounded-md text-center p-10 absolute z-10 center">
     <h1 class="text-5xl font-bold tracking-wide text-red-800">HOLD UP!!!</h1>
     <p class="text-2xl mt-5 mb-5">We’re preparing a <span class="text-sky-400 italic">new batch</span> of projects just for you! Grab a cup of coffee, sit back, and unwind. Let us handle the heavy lifting for you. ☕🌟</p>
     <p class="text-sm italic">Once we’re ready this pop up will vanish!</p>
